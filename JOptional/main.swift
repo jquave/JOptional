9c1a0e1b6fde3b2b6c8f9c959a92170b4e7c695e
Jameson Quave <jquave@gmail.com>
2014-12-06 21:32:18 -0600

    Part 2

--- JOptional/main.swift
+++ JOptional/main.swift
@@ -8,7 +8,21 @@
 
 import Foundation
 
-enum JOptional<T> {
+func == <T>(lhs: JOptional<T>, rhs: JOptional<T>) -> Bool {
+    switch (lhs,rhs) {
+    case (.Some(let lhsVal), .Some(let rhsVal)):
+        // Both have a value, the *optionals* are equal, although the values might not be
+        return true
+    case (.None, .None):
+        // Both are nil
+        return true
+    default:
+        // One does not have a value, but the other does
+        return false
+    }
+}
+
+enum JOptional<T>: Equatable, NilLiteralConvertible {
     case None
     case Some(T)
     
@@ -25,9 +39,16 @@ enum JOptional<T> {
         case .Some(let x):
             return x
         default:
-            assert(true, "Unexpectedly found nil while unwrapping an JOptional value")
+            fatalError("Unexpectedly found nil while unwrapping an JOptional value")
         }
-        return JOptional.None
+    }
+    
+    var description: String {
+        return "hi"
+    }
+    
+    init(nilLiteral: ()) {
+        self = .None
     }
     
 }
@@ -37,10 +58,37 @@ postfix func >! <T>(value: JOptional<T> ) -> Any {
     return value.unwrap()
 }
 
+// Part1
 // Instantiate some optionals
 var myOptionalString = JOptional("A String!")
-var a = myOptionalString.unwrap()   // "A String!"
-var c = myOptionalString>!          // "A String!"
+var a = myOptionalString.unwrap() // "A String!"
+var c = myOptionalString>!     // "A String!"
 
 println(a)
 println(c)
+
+
+// Part 2
+
+// Using Swift's Optional
+var myNilValue : String? = nil
+if(myNilValue == nil) {
+    println("This Optional is nil!")
+}
+else {
+    println("This Optional isn't nil, carry on")
+}
+
+// Using JOptional
+var x:Bool? = nil
+var myNilJOptional = JOptional(x)
+
+myNilJOptional = nil
+
+if(myNilJOptional == nil) {
+    println("This JOptional is nil!")
+}
+else {
+    println("This JOptional isn't nil, carry on")
+}
+
