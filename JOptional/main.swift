20009bd0516699e55f42144491c82606b3072baa
Jameson Quave <jquave@gmail.com>
2014-12-06 20:39:10 -0600

    Initial Commit

---
+++ JOptional/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JOptional
+//
+//  Created by Jameson Quave on 12/6/14.
+//  Copyright (c) 2014 JQ Software LLC. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
