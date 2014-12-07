f65cb5cfa87f666416675ea7994ecbef3ae38b0c
Jameson Quave <jquave@gmail.com>
2014-12-06 20:44:41 -0600

    Part 1

--- JOptional/main.swift
+++ JOptional/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-println("Hello, World!")
+enum JOptional<T> {
+    case None
+    case Some(T)
+    
+    init(_ value: T) {
+        self = .Some(value)
+    }
+    
+    init() {
+        self = .None
+    }
+    
+    func unwrap() -> Any {
+        switch self {
+        case .Some(let x):
+            return x
+        default:
+            assert(true, "Unexpectedly found nil while unwrapping an JOptional value")
+        }
+        return JOptional.None
+    }
+    
+}
 
+postfix operator >! {}
+postfix func >! <T>(value: JOptional<T> ) -> Any {
+    return value.unwrap()
+}
+
+// Instantiate some optionals
+var myOptionalString = JOptional("A String!")
+var a = myOptionalString.unwrap()   // "A String!"
+var c = myOptionalString>!          // "A String!"
+
+println(a)
+println(c)
